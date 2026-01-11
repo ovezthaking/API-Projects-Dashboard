252e076ca357fd9e7f991b5e825c1e578ee3fbe7
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-01-11 18:38:38 +0100

    Add: enhance activity display on button click with new styles and transitions

--- bored-bot/index.js
+++ bored-bot/index.js
@@ -5,5 +5,10 @@ const button = document.querySelector('button')
 button.addEventListener('click', () => {
     fetch('https://apis.scrimba.com/bored/api/activity')
         .then(response => response.json())
-        .then(data => activityText.textContent = data.activity)
+        .then(data => {
+            activityText.textContent = data.activity
+            activityText.classList.add('activity-shown-h4')
+            document.querySelector('h1').textContent = '💡🧠💡🧠💡'
+            document.querySelector('body').classList.add('activity-shown-body')
+        })
 })
