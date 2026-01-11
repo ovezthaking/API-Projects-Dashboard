8d28cfe05a5876ad56302d0309e9b64c06883c90
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-01-11 18:09:59 +0100

    Add: implement activity fetching on button click

--- bored-bot/index.js
+++ bored-bot/index.js
@@ -2,4 +2,8 @@ const activityText = document.querySelector('h4')
 const button = document.querySelector('button')
 
 
-
+button.addEventListener('click', () => {
+    fetch('https://apis.scrimba.com/bored/api/activity')
+        .then(response => response.json())
+        .then(data => activityText.textContent = data.activity)
+})
