bc6667077a49d3bff680739c07fa328722e08647
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-01-12 02:06:12 +0100

    Add: implement posts fetching and display in Blog Space

--- blog-space/index.js
+++ blog-space/index.js
@@ -1,3 +1,13 @@
+const postsContainer = document.getElementById('posts-container')
+
+
 fetch('https://apis.scrimba.com/jsonplaceholder/posts')
     .then(response => response.json())
-    .then(data => console.log(data.slice(0,5)))
+    .then(data => {
+        data.slice(0,5).forEach(post => {
+            postsContainer.innerHTML += `
+                <h2>${post.title}</h2>
+                <p>${post.body}</p>
+            `
+        });
+    })
