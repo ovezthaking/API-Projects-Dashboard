94ee27cc20db045ebad177bcadf68870b7f77d17
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-01-12 02:58:20 +0100

    Add: implement form submission for creating new posts

--- blog-space/index.js
+++ blog-space/index.js
@@ -1,6 +1,21 @@
 const postsContainer = document.getElementById('posts-container')
 
 
+document.addEventListener('submit', (e) => {
+    e.preventDefault()
+    if (e.target.id == document.querySelector('button').id){
+        if (
+            document.getElementById('post-title').value &&
+            document.getElementById('post-body').value) {
+                const post = {
+                    title: document.getElementById('post-title').value,
+                    body: document.getElementById('post-body').value
+                }
+                console.log(post)
+            }
+    }
+})
+
 fetch('https://apis.scrimba.com/jsonplaceholder/posts')
     .then(response => response.json())
     .then(data => {
