2964c93f45e15107a1d28c0da509efd8a6c98095
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-01-14 00:30:09 +0100

    Add: create initial HTML and JavaScript files for card deck shuffling functionality

---
+++ war/index.js
@@ -0,0 +1,5 @@
+document.getElementById('new-deck').addEventListener('click', ()=> {
+    fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
+        .then(res => res.json())
+        .then(data => console.log(data))
+})
