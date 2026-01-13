48584b4ca1927e29e651ef9215b353d8ad205647
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-01-14 00:42:42 +0100

    Update: change card game link icon and refactor event listener for new deck button

--- war/index.js
+++ war/index.js
@@ -1,5 +1,7 @@
-document.getElementById('new-deck').addEventListener('click', ()=> {
+const handleClick = () => {
     fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
         .then(res => res.json())
         .then(data => console.log(data))
-})
+}
+
+document.getElementById('new-deck').addEventListener('click', handleClick)
