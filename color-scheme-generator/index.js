d9de14a3fa37bd14ca70c6b39d314c271f865ab7
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-01-13 05:47:34 +0100

    Add: implement color scheme generator functionality with API integration

---
+++ color-scheme-generator/index.js
@@ -0,0 +1,35 @@
+const main = document.querySelector('main')
+
+function fetchApi(hexValue){
+    const mode = document.getElementById('color-mode').value
+    fetch(`https://www.thecolorapi.com/scheme?hex=${hexValue}&mode=${mode}`)
+        .then(res => res.json())
+        .then(data => {
+            data.colors.forEach(color => {
+                console.log(color.hex.value)
+                const container = document.createElement('div')
+                container.classList.add('color-container')
+                const div = document.createElement('div')
+                div.style.backgroundColor = `${color.hex.value}`
+                div.style.height = '100%'
+                const p = document.createElement('p')
+                p.textContent = color.hex.value
+                main.append(container)
+                container.append(div)
+                container.append(p)
+            })
+        })
+}
+
+function render() {
+    fetchApi('000000')
+}
+
+document.addEventListener('submit', (e) => {
+    e.preventDefault()
+    main.innerHTML = ''
+    const hexValue = document.getElementById('pick-color').value.slice(1)
+    fetchApi(hexValue)
+})
+
+render()
