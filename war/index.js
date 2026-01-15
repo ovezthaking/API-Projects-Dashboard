f25ea6e34e592f680857def6d4bcd5c8050f4b09
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-01-15 01:55:28 +0100

    Add: implement winner determination logic and update score display

--- war/index.js
+++ war/index.js
@@ -1,10 +1,23 @@
 let deckId
 const cardsContainer = document.getElementById('cards')
+const scoreHeader = document.getElementById('score')
 
 const handleClick = () => {
     fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
         .then(res => res.json())
         .then(data => deckId = data.deck_id)
+}
+
+const winnerCard = (card1, card2) => {
+    const values = ["2", "3", "4", "5", "6", "7", "8", "9", 
+        "10", "JACK", "QUEEN", "KING", "ACE"]
+    
+    const card1Index = values.indexOf(card1.value)
+    const card2Index = values.indexOf(card2.value)
+
+    if (card1Index > card2Index) scoreHeader.innerText = 'Computer wins!'
+    else if(card2Index > card1Index) scoreHeader.innerText = 'You win!'
+    else scoreHeader.innerText = 'War!'
 }
 
 const draw = () => {
@@ -18,6 +31,7 @@ const draw = () => {
             cardsContainer.children[1].innerHTML = `
                 <img src="${cards[1].image}" alt="card 2 image" class="card">
             `
+            winnerCard(cards[0], cards[1])
         })
 }
 
