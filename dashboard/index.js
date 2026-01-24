ad97d5fa2b372cdd7432b36103d3565d55eda4c2
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-01-24 02:37:04 +0100

    Update: fix author variable assignment and improve background rendering logic

--- dashboard/index.js
+++ dashboard/index.js
@@ -1,4 +1,5 @@
 let imgUrl = ''
+author = ''
 
 
 const renderBackground = async () => {
@@ -8,12 +9,14 @@ const renderBackground = async () => {
         imgUrl = await data.urls.full
         console.log(imgUrl)
 
-        document.getElementById('author').innerText = `By: ${data.user.name}`
+        author = data.user.name
     } catch (e) {
         imgUrl = 'https://images.unsplash.com/photo-1519681393784-d120267933ba?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        author = 'Benjamin Voros'
     }
     finally {
         document.body.style.backgroundImage = `url('${imgUrl}')`
+        document.getElementById('author').textContent = `By: ${author}`
     }
 }
 
