8a7e2896498adc279ebc77d54e1964d920f972c9
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-01-24 03:44:45 +0100

    Update: add geolocation-based weather data fetching with error handling

--- dashboard/index.js
+++ dashboard/index.js
@@ -57,6 +57,21 @@ const renderTime = () => {
 }
 
 
+navigator.geolocation.getCurrentPosition(position => {
+    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather/?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
+        .then(res => {
+            if (!res.ok) {
+                throw Error('Weather data not available!')
+            }
+            return res.json()
+        })
+        .then(data => {
+            console.log(data)
+        })
+        .catch(e => console.error('Error getting weather data: ', e))
+});
+
+
 renderBackground()
 renderCrypto()
 setInterval(renderTime, 1000);
