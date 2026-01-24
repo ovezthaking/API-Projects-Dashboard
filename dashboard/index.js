f822fa628aab348f901a544ffc9b4d4100624d29
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-01-24 01:03:10 +0100

    Update: implement background image rendering from Unsplash API

---
+++ dashboard/index.js
@@ -0,0 +1,10 @@
+const renderBackground = async () => {
+    const res = await fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature')
+    const data = await res.json()
+    const imgUrl = await data.urls.full
+    
+    document.body.style.backgroundImage = `url('${imgUrl}')`
+}
+
+
+renderBackground()
