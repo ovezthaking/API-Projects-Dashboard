985579a985dacbf50139132fe2602c6cc1f35a40
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-01-24 01:45:06 +0100

    Update: fix imgUrl scope issue and streamline background rendering logic

--- dashboard/index.js
+++ dashboard/index.js
@@ -1,17 +1,34 @@
+let imgUrl = ''
+
+
 const renderBackground = async () => {
     try {
         const res = await fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature')
         const data = await res.json()
-        const imgUrl = await data.urls.full
+        imgUrl = await data.urls.full
         console.log(imgUrl)
 
-        document.body.style.backgroundImage = `url('${imgUrl}')`
         document.getElementById('author').innerText = `By: ${data.user.name}`
     } catch (e) {
-        const defaultBgUrl = 'https://images.unsplash.com/photo-1519681393784-d120267933ba?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-        document.body.style.backgroundImage = `url('${defaultBgUrl}')`
+        imgUrl = 'https://images.unsplash.com/photo-1519681393784-d120267933ba?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     }
-    
+    finally {
+        document.body.style.backgroundImage = `url('${imgUrl}')`
+    }
+}
+
+
+const renderCrypto = async () => {
+    try {
+        const res = await fetch('')
+        const data = await res.json()
+        console.log(data)
+    } catch (e) {
+        console.error('Error fetching crypto', e)
+    }
+    finally {
+
+    }
 }
 
 
