c441a5233f1261feff3b8959564b145bdaaf1956
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-01-24 01:13:55 +0100

    Update: enhance background rendering and display author information

--- dashboard/index.js
+++ dashboard/index.js
@@ -2,8 +2,9 @@ const renderBackground = async () => {
     const res = await fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature')
     const data = await res.json()
     const imgUrl = await data.urls.full
-    
+    console.log(data)
     document.body.style.backgroundImage = `url('${imgUrl}')`
+    document.getElementById('author').innerText = `By: ${data.user.name}`
 }
 
 
